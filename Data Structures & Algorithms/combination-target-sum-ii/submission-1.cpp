62aab7b5c93ef775102d566b451ef198f7ddbe27
Kavish Garg <kavishlegend@gmail.com>
2026-09-01 14:23:59 +0530

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void f(int i,vector<int>& nums,vector<int>&temp,vector<vector<int>>&ans, int sum,int tar)
+    {
+        if(sum==tar)
+        {ans.push_back(temp);
+        return;}
+        for(int j=i;j<nums.size();j++)
+        {
+            if(j > i && nums[j] == nums[j-1]) continue;
+            if(sum + nums[j] > tar) break;
+            temp.push_back(nums[j]);
+            f(j+1,nums,temp,ans,sum+nums[j],tar);
+            temp.pop_back();
+        }
+        return ;
+    }
+    vector<vector<int>> combinationSum2(vector<int>& nums, int tar) 
+    {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        f(0,nums,temp,ans,0,tar);
+        return ans;
+    }
+};
