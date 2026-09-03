23133ca3d6f5381afc87f6de59e2554d8610ad7e
Kavish Garg <kavishlegend@gmail.com>
2026-09-03 22:44:55 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,83 @@
+// class Solution {
+// public:
+//     int dx[4]={0,1,0,-1};
+//     int dy[4]={1,0,-1,0};
+//     void dfs(int r,int c,vector<vector<char>>& grid,vector<vector<int>>&vis)
+//     {
+//         vis[r][c]=1;
+//         for(int i=0;i<4;i++)
+//         {
+//             int nr=dx[i]+r;
+//             int nc=dy[i]+c;
+
+//             if(nr>=grid.size() || nc>=grid[0].size() || nr<0 || nc<0 
+//             || vis[nr][nc] || grid[nr][nc]=='0')
+//             continue;
+
+//             dfs(nr,nc,grid,vis);
+//         }
+//         return ;
+//     }
+//     int numIslands(vector<vector<char>>& grid) 
+//     {
+//         int n=grid.size();
+//         int m=grid[0].size();
+//         vector<vector<int>>vis(n,vector<int>(m,0));
+//         int c=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<m;j++)
+//             {
+//                 if(grid[i][j]=='1' && !vis[i][j])
+//                 {
+//                     dfs(i,j,grid,vis);
+//                     c++;
+//                 }
+//             }
+//         }
+//         return c;
+
+//     }
+// };
+
+class Solution {
+public:
+    int dx[4]={0,1,0,-1};
+    int dy[4]={1,0,-1,0};
+    
+    void dfs(int r, int c, vector<vector<char>>& grid)
+    {
+        grid[r][c] = '0'; 
+        
+        for(int i = 0; i < 4; i++)
+        {
+            int nr = dx[i] + r;
+            int nc = dy[i] + c;
+
+            if(nr >= grid.size() || nc >= grid[0].size() || nr < 0 || nc < 0 || grid[nr][nc] == '0')
+                continue;
+
+            dfs(nr, nc, grid);
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        int c = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    dfs(i, j, grid);
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+};
