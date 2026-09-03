6b6e27fa0ea5353da75fd88bde050b992bd31ff2
Kavish Garg <kavishlegend@gmail.com>
2026-09-03 22:11:22 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    vector<string>c={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void f(int idx, string temp,vector<string>&ans,string &digits)
+    {
+        int n=digits.size();
+        if(idx==n)
+        {
+            if(temp.size()==n)
+            ans.push_back(temp);
+            return;
+        }
+        int num=digits[idx]-'0';
+        for(int i=0;i<c[num].size();i++)
+        {
+            temp.push_back(c[num][i]);
+            f(idx+1,temp,ans,digits);
+            temp.pop_back();
+        }
+        return;
+    }
+    vector<string> letterCombinations(string digits) 
+    {
+        vector<string>ans;
+        string temp="";
+        if(digits=="")
+        return ans;
+
+            f(0,temp,ans,digits);
+
+        return ans;
+    }
+};
